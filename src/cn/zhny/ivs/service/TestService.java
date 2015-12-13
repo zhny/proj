784ccc2401234e104bbffa9ddb85055946b07abe
zhny <461787231@qq.com>
2015-12-13 19:35:11 +0800

    呵呵呵呵

--- src/cn/zhny/ivs/service/TestService.java
+++ src/cn/zhny/ivs/service/TestService.java
@@ -1,10 +1,15 @@
 package cn.zhny.ivs.service;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 
+import cn.zhny.base.utils.Digests;
 import cn.zhny.ivs.utils.CmsServiceUtil;
+import cn.zhny.ivs.webservice.CmsService;
+import cn.zhny.ivs.webservice.CmsServicePortType;
 import cn.zhny.ivs.webservice.CommonPageReq;
 import cn.zhny.ivs.webservice.DeviceResult;
+import cn.zhny.ivs.webservice.LoginResult;
 
 public class TestService {
 	
